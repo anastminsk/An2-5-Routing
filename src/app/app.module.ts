92255b8b5fc796f27c9a9e3c4ff1a29687673eca
anastminsk <anast-minsk@yandex.ru>
2021-01-06 17:53:43 +0300

    add base routing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    LayoutModule,
+    // MUST BE LAST
+    AppRoutingModule
   ],
   providers: [
   ],

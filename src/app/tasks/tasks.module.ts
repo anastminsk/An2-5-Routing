7b26fd3eda2b703fbcaf52b7caa9619d820636eb
anastminsk <anast-minsk@yandex.ru>
2021-09-09 09:48:26 +0300

    add task feature module and model

---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TasksRoutingModule } from './tasks-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TasksRoutingModule,
+  ],
+})
+export class TasksModule { }

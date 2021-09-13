408dda7c25877ef157db43d073545122a76457a7
anastminsk <anast-minsk@yandex.ru>
2021-09-13 09:12:41 +0300

    get route parameter and navigate

--- src/app/tasks/components/task-form/task-form.component.ts
+++ src/app/tasks/components/task-form/task-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { TaskModel } from './../../models/task.model';
 import { TaskArrayService } from './../../services/task-array.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './task-form.component.html',
@@ -10,10 +12,25 @@ import { TaskArrayService } from './../../services/task-array.service';
 export class TaskFormComponent implements OnInit {
   task: TaskModel;
 
-  constructor(private taskArrayService: TaskArrayService) {}
+  constructor(
+    private taskArrayService: TaskArrayService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.task = new TaskModel();
+
+    // it is not necessary to save subscription to route.paramMap
+    // when router destroys this component, it handles subscriptions automatically
+    const observer = {
+      next: (task: TaskModel) => (this.task = {...task}),
+      error: (err: any) => console.log(err),
+    };
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => this.taskArrayService.getTask(+params.get('taskID'))))
+      .subscribe(observer);
   }
 
   onSaveTask() {
@@ -24,7 +41,11 @@ export class TaskFormComponent implements OnInit {
     } else {
       this.taskArrayService.createTask(task);
     }
+
+    this.onGoBack();
   }
 
-  onGoBack(): void {}
+  onGoBack(): void {
+    this.router.navigate(['/home']);
+  }
 }

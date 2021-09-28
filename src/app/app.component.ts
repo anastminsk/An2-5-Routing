15bbc7a72e9dc035de98a46c3fb1a98348714df4
anastminsk <anast-minsk@yandex.ru>
2021-09-28 11:39:22 +0300

    implement secondary router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessagesService } from './core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    public messagesService: MessagesService,
+    private router: Router
+  ) {}
+
+  onDisplayMessages(): void {
+    this.router.navigate([{ outlets: { messages: ['messages'] } }]);
+    this.messagesService.isDisplayed = true;
+  }
+}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AboutComponent, PathNotFoundComponent } from './components';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent, MessagesComponent, PathNotFoundComponent } from './components';
 
 @NgModule({
   declarations: [
     AboutComponent,
+    MessagesComponent,
     PathNotFoundComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
   ],
 })
 export class LayoutModule { }
